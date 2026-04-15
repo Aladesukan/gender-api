d8ef6cd51e70fa3fa06d3daa4c901570def5f07e
Aladesukan <aladesukanf@gmail.com>
2026-04-15 01:36:32 +0100

    testing trials

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ClassifyController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/trial', function(){
+    return response()->json(['message' => 'Api is working']);
+});
+
+Route::get('/classify', [ClassifyController::class, 'classify'])->name('classify');
